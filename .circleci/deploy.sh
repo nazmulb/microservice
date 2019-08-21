0a15c13b0e142bd14319d10ab81898b25a568412
nazmulbasher <nazmul.basher@fieldnation.com>
2019-08-21 14:54:24 +0600

    Changed some codes

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -5,5 +5,3 @@ docker pull nazmulb/microservice
 docker run -d -p 8082:8080 --name nazmul_micro nazmulb/microservice
 
 sleep 5
-
-docker exec nazmul_micro bash -c 'curl http://localhost:8082/about'
