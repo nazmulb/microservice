88103ee47011bd83dff2d5a72023873a6a8cd58f
nazmulbasher <nazmul.basher@fieldnation.com>
2019-08-21 13:24:26 +0600

    Changed some codes

--- is_healthy.sh
+++ is_healthy.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 set -e
 
-i=0
-until wget -q --tries=20 --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 --spider http://localhost:8082
-do
-  if [ $((++i)) -gt 30 ]; then
-    echo "Timed out waiting for monolith";
-    exit 1;
-  fi
-  echo "Waiting for Monolith app ..."
-  sleep 5
-done
+docker exec nazmul_micro bash -c 'curl http://localhost:8082/about'
